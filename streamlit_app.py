695d5e44dfdffe9d5432b4f02a1f0edf2677549b
David-FPI <156213645+David-FPI@users.noreply.github.com>
2024-09-16 13:10:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,45 @@
 import streamlit as st
 
+# Title of the app
 st.title("🎈 A-Flamingo Club")
-st.image("https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-6/447678075_767488495531324_3390405170555493979_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=xuaEcEZMd6sQ7kNvgHLiqBW&_nc_ht=scontent.fsgn5-8.fna&_nc_gid=AWVa4ylzPoVZW2g61tLsXxo&oh=00_AYCzt7Ed6xiewhnBUFT4nEFDwYj9gjdXoQBX_fyHNLYGEw&oe=66EDA697", caption="California Housing Data ")
+
+# Custom CSS to make the image circular and align the image and description side by side
+st.markdown(
+    """
+    <style>
+    .container {
+        display: flex;
+        align-items: center;
+    }
+    .icon {
+        border-radius: 50%;
+        width: 150px;
+        height: 150px;
+        margin-right: 20px;
+    }
+    .description {
+        font-size: 20px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Display image and description using HTML for layout
+st.markdown(
+    """
+    <div class="container">
+        <img src="https://scontent.fsgn5-8.fna.fbcdn.net/v/t39.30808-6/447678075_767488495531324_3390405170555493979_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=xuaEcEZMd6sQ7kNvgHLiqBW&_nc_ht=scontent.fsgn5-8.fna&_nc_gid=AWVa4ylzPoVZW2g61tLsXxo&oh=00_AYCzt7Ed6xiewhnBUFT4nEFDwYj9gjdXoQBX_fyHNLYGEw&oe=66EDA697" class="icon">
+        <div class="description">
+            <strong>California Housing Data</strong><br>
+            Explore the housing data in California with visualizations and insights!
+        </div>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+# Call to action link
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
